fbb0e17223114cc118c9f23cae88d1d016adf19e
martin5 <martin5@github.com>
2012-11-13 20:26:26 +0100

    Kreirana funkcijata zbir_recip_vr()

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int izbor();
 void povikaj(int);
 //void br_deliteli();
-//void zbir_recip_vr();
+void zbir_recip_vr();
 
 
 //	--Glavniot vlez vo programata--
@@ -39,12 +39,31 @@ void povikaj(int izbr)
 	switch(izbr)
 	{
 		case 1:
-			//povikaj ja br_deliteli()
+			//povikaj ja br_deliteli();
 			break;
 		case 2:
-			//povikaj ja zbir_recip_vr()
+			zbir_recip_vr();
 			break;
 		default:
 			break;
 	}
+}
+
+//	--Funkcija za presmetka na zbirot na reciprocnite vrednosti na prvite n-broevi
+void zbir_recip_vr()
+{
+	float sum=0, n;
+	cout<<"Do koj br da presmetam? ";
+	cin>>n;
+
+	if(n<1)	//ako NE E vnesen PRIRODEN broj ; za broevi pomali od 1
+	{
+		cout<<"*****Brojot treba da bidi pogolem od 0."<<endl;
+		cin>>n;
+	}
+	for(float i=1;i<=n;i++)
+	{
+		sum=sum+(1/i);
+	}
+	cout<<"Zbirot na reciprocnite vrednosti na prvite "<<(int)n<<" prirodni broevi e "<<sum<<endl;
 }
